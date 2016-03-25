bdd29836eaf2392787438ed7dad2ca0fc704e88a
David <ddiehr@yahoo.com>
2016-03-25 08:13:42 -0700

    Initial commit.

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+
+
+/////////////////         root        ///////////////////////
+@Component({
+  selector: 'my-app',
+  directives: [],
+  template: `
+  <div class="container">
+    <h1 class='jumbotron'>Hello World</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+  }
